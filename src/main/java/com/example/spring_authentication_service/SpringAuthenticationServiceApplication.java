28c49930c82803f26107ee1685a2262d7a6e6cc3
tanmay777 <tanmay.jha1@gmail.com>
2019-05-01 23:40:58 +0530

    Init Commit

---
+++ src/main/java/com/example/spring_authentication_service/SpringAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_authentication_service/SpringAuthenticationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_authentication_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAuthenticationServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
